2b11946288072e303eaad918986b0cf0878bd094
sponkurtus2 <a1122150057@utch.edu.mx>
2024-02-03 23:00:02 -0600

    Added: tesstApp

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -5,11 +5,10 @@ import com.github.ricksbrown.cowsay.Cowsay;
 public class App {
   public static void main(String[] args) {
     int result = Calculator.add(5, 5);
-
     // String from cowsay.say method
-    String[] cowArgs = new String[] { String.valueOf(result) };
-    String cowString = Cowsay.say(cowArgs);
-    System.out.println(cowString);
+    // String[] cowArgs = new String[] { String.valueOf(result) };
+    // String cowString = Cowsay.say(cowArgs);
+    // System.out.println(cowString);
 
   }
 }
---
+++ my-app/src/main/java/com/mycompany/app/TestToAdd.java
@@ -0,0 +1,7 @@
+package com.mycompany.app;
+
+public class TestToAdd {
+  public void sayBye() {
+    System.out.println("Byee");
+  }
+}
